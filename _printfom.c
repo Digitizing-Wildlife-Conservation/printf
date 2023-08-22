b32442985e579282ad8af2216171dead6e1b71df
Digitizing-Wildlife-Conservation <fromthebooksofsamngethe@gmail.com>
2023-08-22 07:21:58 -0700

    Muchai printf Tasks

--- _printfom.c
+++ _printfom.c
@@ -1,51 +1,30 @@
-#include <stdarg.h>
 #include "main.h"
 
 /**
- *_printf - prints to output according to format
- *@format: character string
+ * _printf - Produces output according to a format
+ * @format: Is a character string. The format string
+ * is composed of zero or more directives
  *OdenyiMuchai Alias OM
- *Return: number of characters printed
- */
-
+ * Return: The number of characters printed (excluding
+ * the null byte used to end output to strings)
+ **/
 int _printf(const char *format, ...)
 {
-	int iom = 0, jom = 0;
-	int (*fom)(va_list);
-	va_list args;
+	int sizeom;
+	va_list argsom;
 
-	va_start(args, format);
-	if (format == NULL || !format[iom + 1])
+	if (format == NULL)
 		return (-1);
-	while (format[iom])
-	{
-		if (format[iom] == '%')
-		{
-			if (format[iom + 1])
-			{
-				if (format[iom + 1] != 'c' && format[iom + 1] != 's'
-				&& format[iom + 1] != '%' && format[iom + 1] != 'd'
-				&& format[iom + 1] != 'i')
-				{
-					jom += _putchar(format[iom]);
-					jom += _putchar(format[iom + 1]);
-					iom++;
-				}
-				else
-				{
-					fom = get_func(&format[iom + 1]);
-					jom += f(args);
-					iom++;
-				}
-			}
-		}
-		else
-		{
-			_putchar(format[iom]);
-			jom++;
-		}
-		iom++;
-	}
-	va_end(args);
-	return (jom);
+
+	sizeom = _strlen(format);
+	if (sizeom <= 0)
+		return (0);
+
+	va_start(argsom, format);
+	sizeom = handler(format, argsom);
+
+	_putchar(-1);
+	va_end(argsom);
+
+	return (sizeom);
 }
